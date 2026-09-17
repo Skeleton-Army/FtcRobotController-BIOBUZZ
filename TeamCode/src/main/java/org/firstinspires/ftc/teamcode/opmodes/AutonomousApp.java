6ddf158ef160d0712a0cf5583fe35f7acd2f7c43
opi3636 <86877850+opi3636@users.noreply.github.com>
2026-09-17 19:22:28 +0300

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonomousApp.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.pedropathing.follower.Follower;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.skeletonarmy.marrow.LynxUtil;
+
+import org.firstinspires.ftc.teamcode.pedro.Constants;
+
+@Autonomous(name="Autonomous", preselectTeleOp="TeleOp")
+public class AutonomousApp extends CommandOpMode {
+    private Follower follower;
+
+    @Override
+    public void initialize() {
+        LynxUtil.setBulkCachingMode(hardwareMap, LynxModule.BulkCachingMode.MANUAL);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        follower = Constants.create(hardwareMap);
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        LynxUtil.clearBulkCache(hardwareMap);
+
+        follower.update();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpApp.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.pedropathing.follower.Follower;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.skeletonarmy.marrow.LynxUtil;
+
+import org.firstinspires.ftc.teamcode.pedro.Constants;
+
+@TeleOp(name="TeleOp")
+public class TeleOpApp extends CommandOpMode {
+    private Follower follower;
+
+    @Override
+    public void initialize() {
+        LynxUtil.setBulkCachingMode(hardwareMap, LynxModule.BulkCachingMode.MANUAL);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        follower = Constants.createWithoutReset(hardwareMap);
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        LynxUtil.clearBulkCache(hardwareMap);
+
+        follower.manual(
+                -gamepad1.left_stick_y,
+                -gamepad1.left_stick_x,
+                -gamepad1.right_stick_x
+        );
+        follower.update();
+    }
+}
