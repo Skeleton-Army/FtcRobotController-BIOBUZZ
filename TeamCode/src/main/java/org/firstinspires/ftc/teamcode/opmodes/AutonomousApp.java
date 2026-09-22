ccc374ddef2e8725fdc90f6866b90a0d047e7591
Skeleton Army <skeleton.army23644@gmail.com>
2026-09-22 19:10:57 +0300

    feat: implement autonomous OpMode  starting point

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonomousApp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonomousApp.java
@@ -1,11 +1,23 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+
+import android.graphics.Point;
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.pedropathing.follower.Follower;
+import com.pedropathing.math.Pose;
+import com.pedropathing.paths.Path;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.seattlesolvers.solverslib.command.ScheduleCommand;
+import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
+import com.seattlesolvers.solverslib.command.WaitCommand;
+import com.seattlesolvers.solverslib.command.WaitUntilCommand;
+import com.seattlesolvers.solverslib.pedroCommand.FollowPathCommand;
 import com.skeletonarmy.marrow.LynxUtil;
 
 import org.firstinspires.ftc.teamcode.pedro.Constants;
@@ -13,15 +25,29 @@ import org.firstinspires.ftc.teamcode.pedro.Constants;
 @Autonomous(name="Autonomous", preselectTeleOp="TeleOp")
 public class AutonomousApp extends CommandOpMode {
     private Follower follower;
+    private Path scorePath;
+    private Path parkPath;
+
+
+    private final Pose startPose = new Pose(58.4795, 9.6037, Math.toRadians(90));
+    private final Pose endPose = new Pose(6.5015, 105.8093, Math.toRadians(180));
 
     @Override
     public void initialize() {
         LynxUtil.setBulkCachingMode(hardwareMap, LynxModule.BulkCachingMode.MANUAL);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        follower = Constants.create(hardwareMap);
-    }
 
+        follower = Constants.create(hardwareMap);
+        follower.setPose(startPose);
+        schedule(
+                new SequentialCommandGroup(
+                        new FollowPathCommand(follower, scorePath),
+                        new InstantCommand(), // Shoot
+                        new WaitCommand(2000),
+                        new FollowPathCommand(follower, parkPath)
+        ));
+    }
     @Override
     public void run() {
         super.run();
