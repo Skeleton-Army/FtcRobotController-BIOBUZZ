2c9fb5f3fddbc347093445c922435f4d3217de2e
Skeleton Army <skeleton.army23644@gmail.com>
2026-09-24 17:51:13 +0300

    Revert "halfway through tuning"
    This reverts commit 25c38e66979a7e759e0da9e72cd69355445baf65.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
@@ -2,94 +2,25 @@ package org.firstinspires.ftc.teamcode.pedro;
 
 import com.pedropathing.algorithm.Foresight;
 import com.pedropathing.algorithm.ForesightConfig;
-import com.pedropathing.controllers.Controller;
 import com.pedropathing.follower.Follower;
-import com.pedropathing.revhub.drivetrains.CoaxialPodConfig;
 import com.pedropathing.revhub.drivetrains.Mecanum;
 import com.pedropathing.revhub.drivetrains.MecanumConfig;
 import com.pedropathing.revhub.localizers.PinpointConfig;
 import com.pedropathing.revhub.localizers.PinpointLocalizer;
-import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
 public class Constants {
-
-    public static MecanumConfig drivetrainConfig = new MecanumConfig(c -> {
-        c.frontLeftName.set("leftFront");
-        c.frontRightName.set("rightFront");
-        c.backLeftName.set("leftBack");
-        c.backRightName.set("rightBack");
-        c.frontLeftDirection.set(DcMotorSimple.Direction.REVERSE);
-        c.frontRightDirection.set(DcMotorSimple.Direction.FORWARD);
-        c.backLeftDirection.set(DcMotorSimple.Direction.REVERSE);
-        c.backRightDirection.set(DcMotorSimple.Direction.FORWARD);
-    });
-
-    public static CoaxialPodConfig rightBack = new CoaxialPodConfig(
+    public static MecanumConfig drivetrainConfig = new MecanumConfig(
             c -> {
-                c.name.set("rightBack");
-                c.motorName.set("rb");
-                c.servoName.set("rbTurn");
-                c.servoEncoderName.set("rbTurnEncoder");
-                c.turnController.set(Controller.pid(0.3, 0, 0.005)
-                        .plus(Controller.proportionalFeedforward(0)));
-                c.driveDirection.set(DcMotorSimple.Direction.FORWARD);
-                c.servoDirection.set(DcMotorSimple.Direction.FORWARD);
+
             }
     );
 
-    public static CoaxialPodConfig leftFront = new CoaxialPodConfig(
+    public static PinpointConfig localizerConfig = new PinpointConfig(
             c -> {
-                c.name.set("leftFront");
-                c.motorName.set("lf");
-                c.servoName.set("lfTurn");
-                c.servoEncoderName.set("lfTurnEncoder");
-                c.turnController.set(Controller.pid(0.3, 0, 0.005)
-                        .plus(Controller.proportionalFeedforward(0)));
-                c.driveDirection.set(DcMotorSimple.Direction.FORWARD);
-                c.servoDirection.set(DcMotorSimple.Direction.FORWARD);
+
             }
     );
-
-    public static CoaxialPodConfig rightFront = new CoaxialPodConfig(
-            c -> {
-                c.name.set("rightFront");
-                c.motorName.set("rf");
-                c.servoName.set("rfTurn");
-                c.servoEncoderName.set("rfTurnEncoder");
-                c.turnController.set(Controller.pid(0.3, 0, 0.005)
-                        .plus(Controller.proportionalFeedforward(0)));
-                c.driveDirection.set(DcMotorSimple.Direction.FORWARD);
-                c.servoDirection.set(DcMotorSimple.Direction.FORWARD);
-            }
-    );
-
-    public static CoaxialPodConfig leftBack = new CoaxialPodConfig(
-            c -> {
-                c.name.set("leftBack");
-                c.motorName.set("lb");
-                c.servoName.set("lbTurn");
-                c.servoEncoderName.set("lbTurnEncoder");
-                c.turnController.set(Controller.pid(0.3, 0, 0.0086)
-                        .plus(Controller.proportionalFeedforward(0)));
-                c.driveDirection.set(DcMotorSimple.Direction.FORWARD);
-                c.servoDirection.set(DcMotorSimple.Direction.FORWARD);
-            }
-    );
-
-    public static PinpointConfig localizerConfig = new PinpointConfig(c -> {
-        c.name.set("pinpoint");
-        c.podType.set(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
-        c.xPodOffset.set(-0.254528);
-        c.yPodOffset.set(-6.889764);
-        c.xPodDirection.set(GoBildaPinpointDriver.EncoderDirection.REVERSED);
-        c.yPodDirection.set(GoBildaPinpointDriver.EncoderDirection.FORWARD);
-        c.globalDistanceUnit.set(DistanceUnit.INCH);
-        c.offsetUnits.set(DistanceUnit.INCH);
-    });
 
     public static ForesightConfig foresightConfig = new ForesightConfig(
             c -> {
